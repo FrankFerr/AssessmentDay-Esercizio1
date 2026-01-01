bbbac5d68379576ec89ffac015af40041c13a30a
Francesco Ferrante <93624594+FrankFerr@users.noreply.github.com>
2026-01-01 11:02:29 +0000

    divisi request, response e handler in file separati

---
+++ src/Backend/Features/Employees/EmployeesListQueryHandler.cs
@@ -0,0 +1,45 @@
+namespace Backend.Features.Employees;
+
+internal class EmployeesListQueryHandler(BackendContext context) : IRequestHandler<EmployeesListQuery, List<EmployeesListQueryResponse>>
+{
+    private readonly BackendContext context = context;
+
+    public async Task<List<EmployeesListQueryResponse>> Handle(EmployeesListQuery request, CancellationToken cancellationToken)
+    {
+        var query = context.Employees.AsQueryable();
+        if (!string.IsNullOrEmpty(request.FirstName))
+            query = query.Where(q => q.FirstName.ToLower().Contains(request.FirstName.ToLower()));
+        if (!string.IsNullOrEmpty(request.LastName))
+            query = query.Where(q => q.LastName.ToLower().Contains(request.LastName.ToLower()));
+
+        var data = await query.OrderBy(q => q.LastName).ThenBy(q => q.FirstName).ToListAsync(cancellationToken);
+        var result = new List<EmployeesListQueryResponse>();
+
+        foreach (var item in data)
+        {
+            var resultItem = new EmployeesListQueryResponse
+            {
+                Id = item.Id,
+                Code = item.Code,
+                FirstName = item.FirstName,
+                LastName = item.LastName,
+                Address = item.Address,
+                Email = item.Email,
+                Phone = item.Phone,
+            };
+
+            var department = await context.Departments.SingleOrDefaultAsync(q => q.Id == item.DepartmentId, cancellationToken);
+            if (department is not null)
+                resultItem.Department = new EmployeesListQueryResponseDepartment
+                {
+                    Code = department.Code,
+                    Description = department.Description
+                };
+
+
+            result.Add(resultItem);
+        }
+
+        return result;
+    }
+}
---
+++ src/Backend/Features/Suppliers/SuppliersListQueryHandler.cs
@@ -0,0 +1,32 @@
+namespace Backend.Features.Suppliers;
+
+internal class SupplierListQueryHandler(BackendContext context) : IRequestHandler<SupplierListQuery, List<SupplierListQueryResponse>>
+{
+    private readonly BackendContext context = context;
+
+    public async Task<List<SupplierListQueryResponse>> Handle(SupplierListQuery request, CancellationToken cancellationToken)
+    {
+        var query = context.Suppliers.AsQueryable();
+        if (!string.IsNullOrEmpty(request.Name))
+            query = query.Where(q => q.Name.ToLower().Contains(request.Name.ToLower()));
+
+        var data = await query.OrderBy(q => q.Name).ToListAsync(cancellationToken);
+        var result = new List<SupplierListQueryResponse>();
+
+        foreach (var item in data)
+        {
+            var resultItem = new SupplierListQueryResponse
+            {
+                Id = item.Id,
+                Name = item.Name,
+                Address = item.Address,
+                Email = item.Email,
+                Phone = item.Phone,
+            };
+            
+            result.Add(resultItem);
+        }
+
+        return result;
+    }
+}
